a3c72edf85b11dc0a0bdeaf77ca3daea7b9d8c21
Pardeep Yadav <pardeep.yadav@prorigosoftware.com>
2023-10-30 12:36:05 +0530

    blog_app_apis

---
+++ src/main/java/com/blog/services/UserService.java
@@ -0,0 +1,17 @@
+package com.blog.services;
+
+
+import java.util.List;
+
+import com.blog.entities.User;
+import com.blog.payload.UserDto;
+
+public interface UserService {
+	
+	UserDto createUser(UserDto user); // method 
+	UserDto updateUser(UserDto user,Integer userId);
+    UserDto getUserById(Integer  userId);
+    List<UserDto>getAllUsers();
+    
+   void deleteUser(Integer userId); 
+}
